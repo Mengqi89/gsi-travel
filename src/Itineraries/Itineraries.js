ad4ff5a3025ecf158fde0c920455dd4397f6d83a
Mengqi Wang <wmq516@gmail.com>
2019-10-18 10:18:54 -0600

    first commit

---
+++ src/Itineraries/Itineraries.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Itineraries() {
+    return (
+        <div className="Itineraries">
+            <h2>Itineraries</h2>
+            <div className="itineraries-container">
+                <div className="itinerary">
+                    Tibet
+                </div>
+                <div className="itinerary">
+                    Yangtze River Cruise
+                </div>
+                <div className="itinerary">
+                    Guilin
+                </div>
+                <div className="itinerary">
+                    Beijing
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Itineraries
---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <ul>
+            <li>Itineraries</li>
+            <li>Why Us</li>
+            <li>Testimonials</li>
+        </ul>
+    )
+}
+
+export default NavBar
