33f0dfd5bf0f989308ae1510c2459398a51c195d
Mengqi Wang <wmq516@gmail.com>
2019-12-08 15:50:17 -0700

    added tour pdfs

--- src/ItinerariesPage/ItinerariesPage.js
+++ src/ItinerariesPage/ItinerariesPage.js
@@ -4,15 +4,15 @@ import './ItinerariesPage.css'
 function ItinerariesPage() {
     return (
         <div id='ItinerariesPage'>
-            <h2>Itineraries</h2>
+            <h2 id='ItinerariesPage-title'>Itineraries</h2>
             <p id='itineraries-paragraph'>We offer classic itineraries that cover both must-sees and additional excursions in China.</p>
             <div className='itineraries-container'>
                 <div className='itinerary'>
-                    <h3>Tibet</h3>
+                    <button className='itinerary-title'><h3>Tibet</h3></button>
                     <div className='tibet itinerary-pic'></div>
                 </div>
                 <div className='itinerary'>
-                    <h3>Yangtze River Cruise</h3>
+                    <button className='itinerary-title'><h3>Yangtze River Cruise</h3></button>
                     <div className='yangtze itinerary-pic'></div>
                 </div>
             </div>
