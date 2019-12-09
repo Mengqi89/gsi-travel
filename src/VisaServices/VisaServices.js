4cac140540fcfb3239cdf7bd8c87afa79417aa72
Mengqi Wang <wmq516@gmail.com>
2019-12-09 11:31:16 -0700

    styled visa services page

--- src/VisaServices/VisaServices.js
+++ src/VisaServices/VisaServices.js
@@ -5,8 +5,11 @@ function VisaServices() {
     return (
         <div id='visa-body'>
             <div id='visa-services'>
-                <h2 id='visa-title'>Visa Services</h2>
-                <p id='visa-paragraph'>We offer visa processing service with the Chinese embassies in the U.S. at an affordable rate for a fast and smooth process. We don’t post rates because it depends on a variety of factors. </p>
+                <div id='visa-container'>
+                    <h2 id='visa-title'>Visa Services</h2>
+                    <p id='visa-paragraph'>We offer visa processing service with the Chinese embassies in the U.S. at an affordable rate for a fast and smooth process. We don’t post rates because it depends on a variety of factors. </p>
+                </div>
+
             </div>
         </div>
 
