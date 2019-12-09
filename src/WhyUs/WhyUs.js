a6c0e5483b615855689dc5920dd175db2adca8be
Mengqi Wang <wmq516@gmail.com>
2019-12-09 12:06:40 -0700

    added WhyUs and testinomials texts

--- src/WhyUs/WhyUs.js
+++ src/WhyUs/WhyUs.js
@@ -5,9 +5,24 @@ function WhyUs() {
     return (
         <div id='WhyUs'>
             <h2>Why us?</h2>
-            <div className='us-image'></div>
             <p>
-                Established since 2008, Great Sea International is a travel company specializing in tours to China and cultural exchange activities. We work with a network of the best hotels, restaurants and transportation providers that offer exceptional service for American travelers. Our clients create memories by experiencing China’s vast cultural and geographical wonders. We look forward to making your trip to China exceptional!
+                Established since 2008, Great Sea Travel specializes in tours to China and cultural exchange activities. We work with a network of the best hotels, restaurants and transportation providers that offer exceptional service for American travelers. Our clients create memories by experiencing China’s vast cultural and geographical wonders. We look forward to making your trip to China exceptional!
+            </p>
+            <h2>What our clients say about us?</h2>
+            <p>
+                “My husband and I went on a tour to China with Yao, including the Yangtze River tour.  Everything about it was a <span>wonderful</span> experience. He had it so <span>well-organized</span> that at every stop people were there waiting for us; our food was <span>delicious</span> and <span>ready</span>, and our buses were <span>on time</span>. I would, and have, recommended this tour company. It far exceeded our expectations! You will not be disappointed.”​  &mdash; <span>Carol & Dennis</span>
+            </p>
+            <p>
+                “My daughter’s experience in China was <span>amazing</span>! The chance to see students in their school environment is worth hours of lectures and videos. My daughter’s love for the language and desire to learn it faster was multiplied by being able to see other kids her age interact with her. We also saw so much <span>history</span> and <span>culture</span> that literally <span>came to life</span>. Seeing the Terra Cotta Warriors in person was <span>mind-blowing</span>. We had a <span>fantastic experience</span> and can’t wait to take our other children who are learning Chinese.” &mdash; <span>Larissa & Aurianna</span>
+            </p>
+            <p>
+                “Our family traveled to China with Great Sea Travel with four children ages 4 to 10. We had a <span>marvelous</span> time! The children still refer <span>excitedly</span> to that vacation in our conversations. Mr. Yao, who directed our tour, often took our six-year-old boy by the hand and gave him a private tour along with the group. That <span>personal touch</span> made all the difference to us. This tour of mainland China was <span>memorable</span>, <span>multifaceted</span>, and never boring or tedious. I would <span>unconditionally recommend</span> any tour directed by Mr. Yao.” &mdash; <span>Terry Family</span>
+            </p>
+            <p>
+                “Our tour to China with Great Sea Travel was <span>more than fabulous</span>! Words cannot express what an <span>opportunity</span> this was for my son and I. From the challenge of climbing the Great Wall to learning the history of the people during our rickshaw ride in Hutong area. We met a <span>sweet family</span> on our rickshaw ride and my son held a pet cricket as well as used the Chinese he’s learned at school! We will definitely be booking with Great Sea International again! &mdash; <span>Carrie & Danny</span>
+            </p>
+            <p>
+                “I trusted Yao to get me and 30 students in and out of Tibet. Tough trips require expert guides. Yao is the best you can get.” &mdash; <span>Susan, Professor at Weber State University</span>
             </p>
         </div>
     )
