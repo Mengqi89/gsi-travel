59a16a13db244869641f97b7e98d873db8a09da3
Mengqi Wang <wmq516@gmail.com>
2019-12-09 13:24:02 -0700

    compressed img

--- src/ItinerariesPage/ItinerariesPage.js
+++ src/ItinerariesPage/ItinerariesPage.js
@@ -8,15 +8,15 @@ function ItinerariesPage() {
             <p id='itineraries-paragraph'>We offer classic itineraries that cover both must-sees and additional excursions in China.</p>
             <div className='itineraries-container'>
                 <div className='itinerary'>
-                    <button className='itinerary-title'><a href='https://github.com/Mengqi89/gsi-travel/raw/master/src/assets/tibet.pdf'>Tibet</a></button>
+                    <a href='https://github.com/Mengqi89/gsi-travel/raw/master/src/assets/tibet.pdf'><button className='itinerary-title'>Tibet</button></a>
                     <div className='tibet itinerary-pic'></div>
                 </div>
                 <div className='itinerary'>
-                    <button className='itinerary-title'><a href='https://github.com/Mengqi89/gsi-travel/raw/master/src/assets/yangtze.pdf'>Yangtze River Cruise</a></button>
+                    <a href='https://github.com/Mengqi89/gsi-travel/raw/master/src/assets/yangtze.pdf'><button className='itinerary-title'>Yangtze River Cruise</button></a>
                     <div className='yangtze itinerary-pic'></div>
                 </div>
                 <div className='itinerary'>
-                    <button className='itinerary-title'><a href='https://github.com/Mengqi89/gsi-travel/raw/master/src/assets/spring-break.pdf'>Spring Break Tour</a></button>
+                    <a href='https://github.com/Mengqi89/gsi-travel/raw/master/src/assets/spring-break.pdf'><button className='itinerary-title'>Spring Break Tour</button></a>
                     <div className='spring-break itinerary-pic'></div>
                 </div>
             </div>
