a3f79f916bfbed458f704efbd02231c3d052de52
Mengqi Wang <wmq516@gmail.com>
2020-03-07 13:36:13 -0700

    added pdf viewer for itineraries

--- src/ItinerariesPage/ItinerariesPage.js
+++ src/ItinerariesPage/ItinerariesPage.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import './ItinerariesPage.css'
+import tibet_itinerary from '../assets/tibet.pdf'
+import yangtze_itinerary from '../assets/yangtze.pdf'
+import china_highlights_itinerary from '../assets/china-highlights.pdf'
+
+function togglePdf(id) {
+    let pdf = document.getElementById(id)
+    if (pdf.style.display === 'none') {
+        pdf.style.display = 'block'
+    } else {
+        pdf.style.display = 'none'
+    }
+}
 
 function ItinerariesPage() {
     return (
@@ -8,21 +20,20 @@ function ItinerariesPage() {
             <p id='itineraries-paragraph'>We offer classic itineraries that cover both must-sees and additional excursions in China.</p>
             <div className='itineraries-container'>
                 <div className='itinerary'>
-                    <div>
-                        <a href='https://github.com/Mengqi89/gsi-travel/raw/master/src/assets/tibet.pdf'><button className='itinerary-title'>Tibet</button></a>
-                    </div>
+                    <button className='itinerary-title' onClick={() => togglePdf('tibet-itinerary-pdf')}>Tibet</button>
+                    <iframe title='Tibet Itinerary' src={tibet_itinerary} width='100%' height='500px' id='tibet-itinerary-pdf'></iframe>
                     <div className='tibet itinerary-pic'></div>
                 </div>
                 <div className='itinerary'>
                     <div>
-                        <a href='https://github.com/Mengqi89/gsi-travel/raw/master/src/assets/yangtze.pdf'><button className='itinerary-title'>Yangtze River Cruise</button></a>
+                        <button className='itinerary-title' onClick={() => togglePdf('yangtze-itinerary-pdf')}>Yangtze River Cruise</button>
                     </div>
+                    <iframe title='Yangtze Itinerary' src={yangtze_itinerary} width='100%' height='500px' id='yangtze-itinerary-pdf'></iframe>
                     <div className='yangtze itinerary-pic'></div>
                 </div>
                 <div className='itinerary'>
-                    <div>
-                        <a href='https://github.com/Mengqi89/gsi-travel/raw/master/src/assets/spring-break.pdf'><button className='itinerary-title'>China Highlights Tour</button></a>
-                    </div>
+                    <button className='itinerary-title' onClick={() => togglePdf('highlights-itinerary-pdf')} >China Highlights Tour</button>
+                    <iframe title='China Highlights Itinerary' src={china_highlights_itinerary} width='100%' height='500px' id='highlights-itinerary-pdf'></iframe>
                     <div className='china-highlights itinerary-pic'></div>
                 </div>
             </div>
